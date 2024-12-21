c467c2844e0cd6c0e7e0325e0c065a7962864929
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2024-12-21 21:12:01 +0530

    update

--- MicroUlter/app/src/main/java/com/example/microulter/MainActivity.java
+++ MicroUlter/app/src/main/java/com/example/microulter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.microulter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnableKeyboard = findViewById(R.id.btnEnableKeyboard);
+
+        // Open keyboard settings on button click
+        btnEnableKeyboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+                startActivity(intent);
+            }
+        });
     }
 }
